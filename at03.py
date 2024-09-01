28b941c04c88d33c2ce586a3bec5db299414d4b1
Camila Evelyn <139063116+MyllaWoo@users.noreply.github.com>
2024-09-01 19:43:16 -0300

    Add files via upload

---
+++ at03.py
@@ -0,0 +1,4 @@
+#Conversor de temperatura
+celcius = float(input('Digite a temperatura em Celcius:'))
+fahrenheit = (celcius * 9/5) + 32
+print(f"{celcius} graus Celcius equivalem a {fahrenheit:.2f}graus Farhrenheit.")
---
+++ at04.py
@@ -0,0 +1,6 @@
+#Fatrorial de um número
+num = int(input('Digite um número inteiro positivo:'))
+fatorial = 1
+for i in range (1, num + 1):
+    fatorial *= 1
+print(fatorial)
